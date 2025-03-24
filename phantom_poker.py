ff7b75e70e03196ccc2ac9c99eba5261f0474826
DevilishDagon <koktim@gmail.com>
2025-03-24 15:07:08 +0100

    Added "count_suits" function

--- phantom_poker.py
+++ phantom_poker.py
@@ -2,35 +2,43 @@ import phantom_poker_decks as decks
 import random
 import phantom_poker_multipliers as multipliers
 
+#General variables
 deck = decks.standard_deck
 hand_size = 8
 hand = []
 selected_hand = []
 selected_cards = []
 played_hand = []
+multiplier = 1
+
+A = 0
+K = 0
+Q = 0
+J = 0
 
 def draw_hand():
     for i in range (len(hand),hand_size):
         card = random.choice(list(deck))
         if card in hand:
-            draw_hand()
+            hand.remove(card)
         hand.append(card)
 
 def select_cards():
-    selected_cards = input(f"Please pick 5 of these cards: {hand} \n")
-    selected_cards = selected_cards.split(",")
+    selected_cards = input(f"Please pick 5 of these cards: {hand} \n").replace(" ","").upper().split(",")
+    if selected_cards:
+        print(selected_cards)
     if len(selected_cards) > 5:
         print("You can't pick more than 5 cards!")
         select_cards()
     for card in selected_cards:
         if card not in hand:
             print(f"You do not have {card}!")
+            selected_hand.clear()
             select_cards()
-        elif card not in selected_hand:
-            selected_hand.append(card)
+        elif card in selected_hand:
+            print("Can't select the same card twice!")
         else:
-            print("Can't pick the same card twice!")
-            select_cards()
+            selected_hand.append(card)
 
 def discard():
     for card in selected_hand:
@@ -49,6 +57,22 @@ def count_value():
         deck.pop(card)
     return value
 
+def count_suits(played_hand):
+    H = 0
+    D = 0
+    S = 0
+    C = 0
+    for card in played_hand:
+        if card[0] == "H":
+            H += 1
+        if card[0] == "D":
+            D += 1
+        if card[0] == "S":
+            S += 1
+        if card[0] == "C":
+            C += 1
+    print(H,D,S,C)
+
 while True:
     draw_hand()
     select_cards()
@@ -57,5 +81,8 @@ while True:
     if choice == "p":
         play()
     elif choice == "d":
+        print(selected_hand)
         discard()
+        print(selected_hand)
+    count_suits(played_hand)
     print(count_value())
